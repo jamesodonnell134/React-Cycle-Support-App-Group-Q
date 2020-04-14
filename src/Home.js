36d61651a2d13ed8bd8b79a39e627b41b14e7320
James O'Donnell <45042660+jamesodonnell134@users.noreply.github.com>
2020-04-14 18:01:49 +0100

    Updated Home.js and About.js pages

--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,18 @@ import { FaHome } from 'react-icons/fa';
 
 export default () => {
     return (
-    <div className="home">
-      <h1><FaHome/>Home</h1>
-      <h2>Group Q's Cycling Support App!</h2>
-        <p className="text-align-justify">Please use the navigation menu on the left to work your way through the app.</p><br/>
-        <p>Our app allows you to: </p>
-        <ul className="check-list">
-            <li>Measure ride distances/times.</li>
-            <li>View your rides.</li><br/>
-            <li>Compare your rides on a leaderboard.</li>
-        </ul>
-    </div>
-  );
+        <div className="home">
+            <h1><FaHome/>Home</h1>
+            <p>Please use the navigation menu on the left to work your way through the app.</p>
+            <p>Our app allows you to: </p>
+            <ul className="check-list">
+                <li>Measure your ride distances and times.</li>
+                <li>Save, view and delete your personal rides.</li>
+                <li>Compare your rides on a leaderboard with other users.</li>
+                <li>Personalise your account profile.</li>
+                <li>Toggle dark and light mode for cycling at different times of day.</li>
+            </ul>
+        </div>
+    );
 };
 
