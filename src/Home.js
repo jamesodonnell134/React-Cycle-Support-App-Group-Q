6cd3573ca7cc4c004e721582110e874a1bf229da
Martin Aleksandrov <kyhar72@gmail.com>
2020-04-14 14:17:55 +0100

    style

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { FaHome } from 'react-icons/fa';
+import {Link} from "react-router-dom";
 
 export default () => {
     return (
     <div className="home">
       <h1><FaHome/>Home</h1>
       <h2>Group Q's Cycling Support App!</h2>
-        <p>Please use the navigation menu on the left to work your way through the app.</p><br/>
+        <p className="text-align-justify">Please use the navigation menu on the left to work your way through the app.</p><br/>
         <p>Our app allows you to: </p>
-        <ul>
+        <ul className="check-list">
             <li>Measure ride distances/times.</li>
-            <li>View your rides.</li>
+            <li>View your rides.</li><br/>
             <li>Compare your rides on a leaderboard.</li>
         </ul>
     </div>
